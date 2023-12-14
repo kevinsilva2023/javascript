5013bf73b0919b1e0a8944339b694d72d4ff25a4
kevinsilva2023 <kevinmarcos280@gmail.com>
2023-12-14 00:09:36 -0300

    mais alguns js

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 59.9
+console.log(`A velocidade do seu carro é ${vel}Km/h`)// consolte.log para digitar algo na tela
+// para executar o cod, apretar F8
+
+if (vel > 60){ // condição simplesconsole.log(`Você ultrapassou a veloicidade permitida. MULTADO`)
+    console.log(`Você ultrapassou a veloicidade permitida. MULTADO`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'eua'
+console.log(`Morando em ${país}`)
+if (país == 'Brasil'){
+    console.log(`Você é Brasileiro`)
+} else {
+    console.log(`Você é Estranngeiro`)
+}
