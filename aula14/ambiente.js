b6ae6e8ab3e556fee0c2828693bb8844f0d38919
kevinsilva2023 <kevinmarcos280@gmail.com>
2023-12-20 20:55:45 -0300

    Create ambiente.js

---
+++ aula14/ambiente.js
@@ -0,0 +1,32 @@
+// Estrutura de repetição com FOR
+
+for(var c = 1; c <= 10; c++){
+    console.log(`Passo ${c}`)
+}
+
+/* 
+------------------------------------------------------------- 
+Estrutura de repetição com teste lógico no final
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+}while(c <=6)
+
+-------------------------------------------------------------
+*/
+
+/*
+------------------------------------------------------------- 
+Estrutura de repetição com teste lógico no inicio
+
+var c = 1
+while (c <= 5) {
+    console.log(`Passo ${c}`)
+    c++ // c++ é a mesma coisa que c = c + 1
+}-
+
+-------------------------------------------------------------
+*/
+
